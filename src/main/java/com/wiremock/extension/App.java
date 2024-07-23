6b2240dba7559698ebb2aacf981a36cc53c726ac
Mithilesh Kannojiya <mithileshkannojiya@gmail.com>
2024-07-23 16:52:49 +0530

    update3

--- src/main/java/com/wiremock/extension/App.java
+++ src/main/java/com/wiremock/extension/App.java
@@ -7,9 +7,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Starting Wiremock Server");
-        // WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig()
-        //     .extensions(new CustomResponseTransformer()).port(8090));
-        //     wireMockServer.start();
+        WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig()
+            .extensions(new CustomResponseTransformer()).port(8090));
+            wireMockServer.start();
     }
-    
 }
